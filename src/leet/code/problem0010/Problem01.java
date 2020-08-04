8c57043b88ec630f9b56deae7c5462316287e83d
wbxing <wenbiaoxing@gmail.com>
2020-08-04 14:30:26 +0800

    p1

---
+++ src/leet/code/problem0010/Problem01.java
@@ -0,0 +1,27 @@
+package leet.code.problem0010;
+
+import java.util.Arrays;
+
+/**
+ * 两数之和
+ * <p>
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ */
+public class Problem01 {
+
+    public static void main(String[] args) {
+        Problem01 problem1 = new Problem01();
+        System.out.println(Arrays.toString(problem1.twoSum(new int[]{1, 0, 1}, 1)));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
