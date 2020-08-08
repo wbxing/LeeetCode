0efaec29ff7213e84f058db6cddccb843224763c
wbxing <wenbiaoxing@gmail.com>
2020-08-08 19:26:24 +0800

    相同的树

---
+++ src/leet/code/problem0100/Problem100.java
@@ -0,0 +1,68 @@
+package leet.code.problem0100;
+
+/**
+ * 相同的树
+ * <p>
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * <p>
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * @author wbxing
+ * @date 2020/08/07
+ */
+public class Problem100 {
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        TreeNode q = new TreeNode(1);
+        // q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        Problem100 problem100 = new Problem100();
+        System.out.println(problem100.isSameTree(null, null));
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null) {
+            return false;
+        }
+        if (q == null) {
+            return false;
+        }
+        if (p == q) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
