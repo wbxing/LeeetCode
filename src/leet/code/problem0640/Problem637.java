889eaeb5a003bd2ceeadf09b0828977edaa89e5a
wbxing <wenbiaoxing@gmail.com>
2020-09-12 10:39:22 +0800

    二叉树的层平均值

---
+++ src/leet/code/problem0640/Problem637.java
@@ -0,0 +1,57 @@
+package leet.code.problem0640;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 637. 二叉树的层平均值
+ *
+ * @author wbxing
+ * @date 2020/09/12
+ */
+public class Problem637 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Double> list = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            double sum = 0;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+
+                TreeNode poll = queue.poll();
+                assert poll != null;
+                sum += poll.val;
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            list.add(sum / size);
+        }
+        return list;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
