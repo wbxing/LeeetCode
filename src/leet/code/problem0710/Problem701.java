23baee17634612d90cb4896a8e63321a085f0d63
wbxing <wenbiaoxing@gmail.com>
2020-09-30 13:42:48 +0800

    二叉搜索树中的插入操作

---
+++ src/leet/code/problem0710/Problem701.java
@@ -0,0 +1,43 @@
+package leet.code.problem0710;
+
+
+/**
+ * 701. 二叉搜索树中的插入操作
+ *
+ * @author wbxing
+ * @date 2020/09/30
+ */
+public class Problem701 {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right =  insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
