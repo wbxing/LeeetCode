fb8837ba6f7c0d3811e69b916d291999d625a0da
wbxing <wenbiaoxing@gmail.com>
2020-10-12 18:25:30 +0800

    二叉搜索树的最小绝对差

---
+++ src/leet/code/problem0530/Problem530.java
@@ -0,0 +1,40 @@
+package leet.code.problem0530;
+
+
+/**
+ * 530. 二叉搜索树的最小绝对差
+ *
+ * @author wbxing
+ * @date 2020/10/12
+ */
+public class Problem530 {
+
+    int result = Integer.MAX_VALUE;
+    int pre = -1;
+
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        getMinimumDifference(root.left);
+        if (pre != -1) {
+            result = Math.min(result, root.val - pre);
+        }
+        pre = root.val;
+        getMinimumDifference(root.right);
+
+        return result;
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
