9000f80eddd9deeab8742484c3732eb8e1d4796f
wbxing <wenbiaoxing@gmail.com>
2020-12-14 17:13:03 +0800

    字母异位词分组

--- src/leet/code/problem0050/Problem49.java
+++ src/leet/code/problem0050/Problem49.java
@@ -21,21 +21,16 @@ public class Problem49 {
         if (strings == null || strings.length == 0) {
             return null;
         }
-        if (strings.length == 1) {
-            List<List<String>> result = new ArrayList<>();
-            result.add(Arrays.asList(strings));
-            return result;
-        } else {
-            Map<String, List<String>> map = new HashMap<>();
-            for (String str : strings) {
-                char[] chars = str.toCharArray();
-                Arrays.sort(chars);
-                String key = new String(chars);
-                List<String> list = map.getOrDefault(key, new ArrayList<>());
-                list.add(str);
-                map.put(key, list);
-            }
-            return new ArrayList<>(map.values());
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strings) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
         }
+        return new ArrayList<>(map.values());
     }
 }
