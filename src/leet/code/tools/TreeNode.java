4cfc78a8ec18fa1ab87f23fd811bc387c3c39545
wbxing <wenbiaoxing@gmail.com>
2022-05-01 14:11:50 +0800

    Definition for a binary tree node.

---
+++ src/leet/code/tools/TreeNode.java
@@ -0,0 +1,24 @@
+package leet.code.tools;
+
+/**
+ * @author wbxing
+ * @date 2022/05/01
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
